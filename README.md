ea93da12ac81612faf9da4e300cf20dd1897e842
Michael Clayton <mostly_mike@hotmail.com>
2014-03-17 10:34:19 -0400

    Added base SQL parsing.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ClaytonDBMS
 ===========
 
-A database management system I made for CS448.
+A database management system I, Michael Clayton, made for CS448.
+
+1. It should be noted that I used the GeneralSQLParser library as the basis of my SQL parsing (which I was told was ok to do).
---
+++ sql/tables.sql
@@ -0,0 +1,44 @@
+/* Michael Clayton 2014 */
+
+CREATE TABLE DEPARTMENT (
+deptid      int             NOT NULL,
+dname       varchar(50),
+location    varchar(50),
+PRIMARY KEY(deptid)
+);
+
+CREATE TABLE STUDENT (
+snum    int                 NOT NULL,
+sname   varchar(50),
+deptid  int,
+slevel  varchar(50),
+age     int,
+FOREIGN KEY(deptid) REFERENCES DEPARTMENT(deptid),
+PRIMARY KEY(snum)
+);
+
+CREATE TABLE FACULTY (
+fid     int                 NOT NULL,
+fname   varchar(50),
+deptid  int,
+FOREIGN KEY(deptid) REFERENCES DEPARTMENT(deptid),
+PRIMARY KEY(fid)
+);
+
+CREATE TABLE CLASS (
+cname       varchar(50)     NOT NULL,
+meets_at    date,
+room        varchar(50),
+fid         int,
+FOREIGN KEY(fid) REFERENCES FACULTY(fid),
+PRIMARY KEY(cname)
+);
+
+CREATE TABLE ENROLLED(
+snum    int                 NOT NULL,
+cname   varchar(50)         NOT NULL,
+PRIMARY KEY(snum, cname),
+FOREIGN KEY(snum) REFERENCES STUDENT(snum),
+FOREIGN KEY (cname) REFERENCES CLASS(cname)
+);
+
